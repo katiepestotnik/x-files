1a468934555068ec8b8100fc9033b7b347685f5f
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-04-06 10:14:22 -0600

    work on combine next

--- helper.js
+++ helper.js
@@ -10,50 +10,69 @@ const port = 3001
 // const shape = terminal.slice(6)
 // const city = terminal.slice(5)
 // console.log(city)
-// const capitalize = (input) => {
-//     const cap = input[0].toUpperCase() + input.slice(1)
-//     return cap
-// }
+const capitalize = (input) => {
+    const cap = input[0].toUpperCase() + input.slice(1)
+    return cap
+}
 // const capLetter = (input) => {
 //     const split = input.split('')
 //     console.log(split)
 // }
-// console.log(capLetter(city))
-// const allCaps = (input) => {
-//     const caps = input.toUpperCase()
-//     return caps
-// }
-// // console.log(capitalize(shape))
-// // console.log(allCaps(state))
-// data.forEach((site) => {
-//     if (site.state === allCaps(state)) {
-//         console.log(site)
-//     }
-//     if (site.shape === capitalize(shape)) {
-//         console.log(site)
-//     }
-//     if (site.city === city) {
-//         console.log(site)
-//     }
-        
-// })
+
+
 const allCaps = (input) => {
     const caps = input.toUpperCase()
     return caps
 }
+// app.get('/', (req, res) => {
+//     res.render('index.ejs', {data:data})
+// })
 
 
 app.get('/sightings', (req, res) => {
     res.json(data)
 })
-app.get("/sightings/:state", (req, res) => {
+app.get("/sightings/:id", (req, res) => {
+    const { state } = req.query
+    const {shape}=req.query
+    //console.log(state)
+    const { id } = req.params
+    if (shape && state) {
+        data.forEach((item) => {
+            if (item.state === id) { 
+                res.render('combine.ejs', { data: data, shape: capitalize(shape), state:(state) })
+                res.redirect("/sightings/id")
+            }
+        })
+    }
+    if (state) {
+        data.forEach((item) => {
+            if (item.state === id) { 
+                res.render('index.ejs', { data: data, state: allCaps(state) })
+                res.redirect("/sightings/id")
+            }
+        })
+    }
+    if (shape) {
+        data.forEach((item) => {
+            if (item.state === id) { 
+                res.render('shapeIndex.ejs', { data: data, shape: capitalize(shape) })
+                res.redirect("/sightings/id")
+            }
+        })
+    }
     data.forEach((item) => {
-        res.render('index.ejs', {data:data, params:req.params.state})
+        if (item.state === id) { 
+            res.render('state.ejs', { data: data, state: allCaps(id) })
+            res.redirect("/sightings/id")
+        }
     })
-
-
-
-
+    data.forEach((item) => {
+        if (item.shape === id) {
+            res.render('shape.ejs', { data: data, shape: capitalize(id)})
+            res.redirect("/sightings/id")
+        }
+    })
 })
 app.listen(port, () => {
         console.log(`Listening on port: ${port}`);
