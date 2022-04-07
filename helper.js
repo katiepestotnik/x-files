74d0a41f75f0f6cfead22f2eb4e26f44a4aac7b8
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-04-07 10:58:51 -0600

    search enabled

--- helper.js
+++ helper.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 const port = 3001
 //console.log(data[0])
-
+app.use(express.urlencoded({extended:false}))
 // const terminal = process.argv[2]
 // console.log('state from terminal', terminal)
 // const state = terminal.slice(-2)
@@ -22,7 +22,6 @@ const allCaps = (input) => {
 app.get('/sightings/', (req, res) => {
     const { state } = req.query
     const { shape } = req.query
-    console.log(shape)
     data.forEach((item) => {
         if (state && shape) {
             res.render('combine.ejs', {data:data, state:allCaps(state),shape:capitalize(shape)})
@@ -34,13 +33,23 @@ app.get('/sightings/', (req, res) => {
             res.render('shapeQuery.ejs', {data:data, shape:capitalize(shape)})
         }
         else if (!shape && !state) {
-            res.json(data)
+            res.render('intro.ejs', {data:data})
         }
         else {
             res.send('error')
         }
         }) 
 })
+app.get("/sightings/new", (req, res) => {
+    res.render('new.ejs')
+})
+
+app.post("/sightings/search", (req, res) => {
+    const { state } = req.body
+    const { shape } = req.body
+    res.render("search.ejs", {data:data, state:allCaps(state), shape:capitalize(shape)})
+})
+
 app.get("/sightings/:id", (req, res) => {
     const { state } = req.query
     const {shape}=req.query
